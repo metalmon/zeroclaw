1b1f7f7adfc3c488020a8a895612ab59d49cf51b
metalmon <propiska63@gmail.com>
2026-08-24 23:57:57 +0400

    test(config): use hygienic test attribute to match schema.rs convention

--- crates/zeroclaw-config/src/schema.rs
+++ crates/zeroclaw-config/src/schema.rs
@@ -25410,7 +25410,7 @@ mod tests {
         );
     }
 
-    #[test]
+    #[::core::prelude::v1::test]
     fn mcp_server_config_eq_detects_arg_change() {
         let a = McpServerConfig {
             name: "kb".into(),
